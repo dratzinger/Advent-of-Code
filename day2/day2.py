317a184fb77615749f83e190bab0e355b168c701
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-02 19:08:07 +0100

    day 2: added skeleton

---
+++ day2/day2.py
@@ -0,0 +1,21 @@
+# --- Day 2: Password Philosophy ---
+
+# --- Part One ---
+def count_valid_passwords(lines):
+    count = 0
+    return count
+
+
+# --- Part Two ---
+
+
+def main():
+    f = open('input.txt')
+    lines = f.readlines()
+    f.close()
+
+    print(count_valid_passwords(lines))
+
+
+if __name__ == '__main__':
+    main()
---
+++ day2/test_day2.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from day2 import count_valid_passwords
+
+KNOWN_INPUT = [
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc"]
+
+
+class Test(TestCase):
+    def test_part_1_known_input(self):
+        self.assertEqual(count_valid_passwords(KNOWN_INPUT), 2)

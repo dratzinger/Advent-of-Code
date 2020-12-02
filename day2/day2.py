0c8f9c256acadec08a2d1a6846771b7e3dfde3fd
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-02 19:45:29 +0100

    day 2 - pt. 1: added implementation

--- day2/day2.py
+++ day2/day2.py
@@ -1,10 +1,28 @@
 # --- Day 2: Password Philosophy ---
 
+def prepare_string(line):
+    line: str
+    strings = line.strip("\n").split(" ")
+    limits = strings[0].split("-")
+    char = strings[1].strip(":")
+    string = strings[2]
+    return [int(limits[0]), int(limits[1]), char, string]
+
+def validate(parameters):
+    min = parameters[0]
+    max = parameters[1]
+    char = parameters[2]
+    count = parameters[3].count(char)
+    return min <= count <= max
+
 # --- Part One ---
 def count_valid_passwords(lines):
     count = 0
+    for line in lines:
+        parameters = prepare_string(line)
+        if validate(parameters):
+            count += 1
     return count
-
 
 # --- Part Two ---
 

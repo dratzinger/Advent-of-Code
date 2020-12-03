529efd8435739f74342b7555b49d1606340b4c2c
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-03 13:05:40 +0100

    day 3 - pt. 1: added unit test

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -2,9 +2,22 @@ from unittest import TestCase
 
 from day3 import count_trees
 
-KNOWN_INPUT = []
+MOVE_RIGHT = 3
+MOVE_DOWN = 1
+KNOWN_INPUT = [
+    "..##.......",
+    "#...#...#..",
+    ".#....#..#.",
+    "..#.#...#.#",
+    ".#...##..#.",
+    "..#.##.....",
+    ".#.#.#....#",
+    ".#........#",
+    "#.##...#...",
+    "#...##....#",
+    ".#..#...#.#"]
 
 
 class Test(TestCase):
     def test_part_1_known_input(self):
-        self.assertEqual(count_trees(KNOWN_INPUT), 0)
+        self.assertEqual(count_trees(KNOWN_INPUT, MOVE_RIGHT, MOVE_DOWN), 7)

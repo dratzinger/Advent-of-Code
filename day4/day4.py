78df59c647ab45fc014f88d2aac5ceb9a451e041
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-04 15:58:08 +0100

    day 4 - pt. 1: added solution

--- day4/day4.py
+++ day4/day4.py
@@ -1,12 +1,46 @@
 # --- Day 4: Passport Processing ---
-def prepare_string(line: str):
-    return line.strip("\n")
+
+REQUIRED_FIELDS = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid"
+]
+
+
+def split_into_pairs(line: str):
+    pairs = []
+    for pair in line.strip("\n").split(" "):
+        pair = pair.split(":")
+        if len(pair) == 2:
+            pairs.append(pair)
+    return pairs
 
 
 # --- Part One ---
 def part_one(lines):
     count = 0
+    data = []
+    for line in lines:
+        pairs = split_into_pairs(line)
+        if len(pairs) < 1:
+            count += int(validate(data))
+            data.clear()
+        else:
+            data.extend(pairs)
     return count
+
+
+def validate(data):
+    required = REQUIRED_FIELDS.copy()
+    for pair in data:
+        key = pair[0]
+        if key in required:
+            required.remove(key)
+    return not len(required)
 
 
 # --- Part Two ---

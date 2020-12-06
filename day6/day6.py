51cfd0883e60036e9972ccc4a39ec8dab05cb64c
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-06 21:41:51 +0100

    day 6 - pt. 2: added solution

--- day6/day6.py
+++ day6/day6.py
@@ -1,30 +1,35 @@
 # --- Day 6: Custom Customs ---
 from util.parser import get_input_grouped
 
-# --- Part One ---
-def count_answers(data):
+def count_answers(data, set_operation):
     counts = []
     for group in data:
-        answers = set()
-        for entry in group:
-            answers = answers.union(set(entry))
+        test = map(lambda entry: set(entry), group)
+        answers = set_operation(*test)
         counts.append(len(answers))
         answers.clear()
     return counts
 
+# --- Part One ---
+def count_answers_any(data):
+    return count_answers(data, set.union)
+
 def sum_answers(data):
-    return sum(count_answers(data))
+    return sum(count_answers_any(data))
+
 
 # --- Part Two ---
-def part_two(lines):
-    count = 0
-    return count
+def count_answers_common(data):
+    return count_answers(data, set.intersection)
+
+def sum_answers_common(data):
+    return sum(count_answers_common(data))
 
 
 def main():
     data = get_input_grouped()
     print(sum_answers(data))
-    print(part_two(data))
+    print(sum_answers_common(data))
 
 
 if __name__ == '__main__':
--- day6/test_day6.py
+++ day6/test_day6.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day6 import sum_answers, part_two
+from day6 import sum_answers, sum_answers_common
 from util.parser import get_input_grouped
 
 
@@ -9,7 +9,7 @@ class Test(unittest.TestCase):
         self.assertEqual(11, sum_answers(get_input_grouped("test_input.txt")))
 
     def test_part_2_known_input(self):
-        self.assertEqual(0, part_two(get_input_grouped("test_input.txt")))
+        self.assertEqual(6, sum_answers_common(get_input_grouped("test_input.txt")))
 
 
 if __name__ == '__main__':

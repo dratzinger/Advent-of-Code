74320eb5bba8a5d520c791aa649df231f2f599d8
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-07 11:05:05 +0100

    day 7: added skeleton

---
+++ day7/day7.py
@@ -0,0 +1,23 @@
+# --- Day 7: Handy Haversacks ---
+from util.parser import get_input_lines
+
+# --- Part One ---
+def part_one(lines):
+    count = 0
+    return count
+
+
+# --- Part Two ---
+def part_two(lines):
+    count = 0
+    return count
+
+
+def main():
+    data = get_input_lines()
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == '__main__':
+    main()

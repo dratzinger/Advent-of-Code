48bfbe75c4d8708e79b2d7f29ecdc4f2542e9b5e
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-09 00:00:16 +0100

    day 8 - pt. 1: WIP

--- day8/day8.py
+++ day8/day8.py
@@ -1,10 +1,45 @@
 # --- Day 8: Handheld Halting ---
+from typing import List, Tuple
+
 from util.parser import get_input_lines
 
+
+class Program(object):
+    def __init__(self, instructions: List[str]) -> None:
+        self.instructions = instructions
+        self.accumulator = 0
+        self.pointer = 0
+        self.jump = 0
+
+    def run(self) -> int:
+        while self.pointer < len(self.instructions):
+            if not self.jump and self.pointer > 0:
+                break
+            inst = self.instructions[self.pointer].split()
+            self.OPS.get(inst[0])(int(inst[1]))
+        return self.accumulator
+
+    def __acc(self, num: int):
+        self.pointer += 1
+        self.accumulator += num
+
+    def __jmp(self, num: int):
+        self.pointer += num
+        self.jump += num
+
+    def __nop(self, num: int):
+        self.pointer += 1
+
+    OPS = {
+        "acc": __acc,
+        "jmp": __jmp,
+        "nop": __nop
+    }
+
+
 # --- Part One ---
-def part_one(data):
-    count = 0
-    return count
+def check_loop_acc_state(data):
+    return Program(data).run()
 
 
 # --- Part Two ---
@@ -15,7 +50,7 @@ def part_two(data):
 
 def main():
     data = get_input_lines()
-    print(part_one(data))
+    print(check_loop_acc_state(data))
     print(part_two(data))
 
 
--- day8/test_day8.py
+++ day8/test_day8.py
@@ -1,11 +1,11 @@
 import unittest
 from util.parser import get_input_lines
-from day8 import part_one, part_two
+from day8 import check_loop_acc_state, part_two
 
 
 class Test(unittest.TestCase):
     def test_part_1_known_input(self):
-        self.assertEqual(5, part_one(get_input_lines("test_input.txt")))
+        self.assertEqual(5, check_loop_acc_state(get_input_lines("test_input.txt")))
 
     def test_part_2_known_input(self):
         self.assertEqual(0, part_two(get_input_lines("test_input.txt")))

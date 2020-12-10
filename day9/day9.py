afbc785856ba327864a56c09ae4b5f26d7998b84
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-10 13:32:11 +0100

    day 9: added skeleton

---
+++ day9/day9.py
@@ -0,0 +1,23 @@
+# --- Day 9: Encoding Error ---
+from util.parser import get_input_lines
+
+# --- Part One ---
+def part_one(data):
+    count = 0
+    return count
+
+
+# --- Part Two ---
+def part_two(data):
+    count = 0
+    return count
+
+
+def main():
+    data = get_input_lines()
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == '__main__':
+    main()

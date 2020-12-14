cbb5f5e14c6d576123477ec08fda8dea2b256c9f
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-14 20:01:59 +0100

    day 8 - pt. 2: WIP loop remover

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,6 @@
 # --- Day 8: Handheld Halting ---
-from typing import List, Tuple
+from copy import deepcopy
+from typing import List
 
 from util.parser import get_input_lines
 
@@ -8,6 +9,7 @@ class Program(object):
     def __init__(self, instructions: List[str]) -> None:
         self.instructions = instructions
         self.executed = [False] * len(instructions)
+        self.jump_stack = []
         self.accumulator = 0
         self.pointer = 0
 
@@ -18,7 +20,7 @@ class Program(object):
         }
 
     def run(self) -> int:
-        while self.pointer < len(self.instructions):
+        while not self.run_completed():
             inst = self.instructions[self.pointer].split()
             op: str = inst[0]
             arg: int = int(inst[1])
@@ -28,11 +30,15 @@ class Program(object):
             self.OPS.get(op)(arg)
         return self.accumulator
 
+    def run_completed(self):
+        return self.pointer+1 >= len(self.instructions)
+
     def __acc(self, num: int):
         self.accumulator += num
         self.pointer += 1
 
     def __jmp(self, num: int):
+        self.jump_stack.append(self.pointer)
         self.pointer += num
 
     def __nop(self, num: int):
@@ -42,6 +48,29 @@ class Program(object):
         self.executed[pointer] = True
 
 
+class JumpLoopRemover(object):
+    def __init__(self, p: Program) -> None:
+        self.program = p
+
+    def troubleshoot(self):
+        self.program.run()
+        loop_removed = self.program.run_completed()
+        stack = self.program.jump_stack
+        while len(stack) > 0 and not loop_removed:
+            change_no = stack.pop()
+            p = self.altered_program_copy(change_no, "nop +0")
+            acc = p.run()
+            loop_removed = p.run_completed()
+            if loop_removed:
+                return acc
+        return "failed to establish a running state by changing single jump"
+
+    def altered_program_copy(self, instruction_no: int, change_to: str):
+        copy = deepcopy(self.program)
+        copy.instructions[instruction_no] = change_to
+        return copy
+
+
 # --- Part One ---
 def check_loop_acc_state(data):
     return Program(data).run()
@@ -49,8 +78,8 @@ def check_loop_acc_state(data):
 
 # --- Part Two ---
 def check_final_acc_state(data):
-    count = 0
-    return count
+    watcher = JumpLoopRemover(Program(data))
+    return watcher.troubleshoot()
 
 
 def main():

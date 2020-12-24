8c9757775ffad7b22c4ba1d21dbe987eb95cdbc2
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-24 13:53:37 +0100

    day 10 - pt. 1: added solution

--- day10/day10.py
+++ day10/day10.py
@@ -1,10 +1,27 @@
 # --- Day 10: Adapter Array ---
+from typing import List
+
 from util.parser import get_input_lines
 
+
+def calculate_difference_distribution(data: List[str]):
+    distribution = [0, 0, 0, 1]  # built-in adapter is always 3 higher than the highest adapter
+    joltages = sorted([int(i) for i in data])
+    previous: int = 0
+    for joltage in joltages:
+        difference = joltage - previous
+        if difference > 3:
+            raise RuntimeWarning("Differences exceed specification")
+        else:
+            distribution[difference] += 1
+        previous = joltage
+    return distribution
+
+
 # --- Part One ---
-def part_one(data):
-    count = 0
-    return count
+def differences_multiplied(data):
+    dist = calculate_difference_distribution(data)
+    return dist[1] * dist[3]
 
 
 # --- Part Two ---
@@ -15,7 +32,7 @@ def part_two(data):
 
 def main():
     data = get_input_lines()
-    print(part_one(data))
+    print(differences_multiplied(data))
     print(part_two(data))
 
 
--- day10/test_day10.py
+++ day10/test_day10.py
@@ -1,11 +1,11 @@
 import unittest
 from util.parser import get_input_lines
-from day10 import part_one, part_two
+from day10 import differences_multiplied, part_two
 
 
 class Test(unittest.TestCase):
     def test_part_1_known_input(self):
-        self.assertEqual(22*10, part_one(get_input_lines("test_input.txt")))
+        self.assertEqual(22 * 10, differences_multiplied(get_input_lines("test_input.txt")))
 
     def test_part_2_known_input(self):
         self.assertEqual(0, part_two(get_input_lines("test_input.txt")))

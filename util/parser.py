5da43400d15d88b829110b4bce7601165735938e
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-24 12:15:16 +0100

    moved newline stripping to separate function

--- util/parser.py
+++ util/parser.py
@@ -2,13 +2,12 @@ import re
 from typing import List
 
 def get_input_lines(filename: str = 'input.txt') -> List[str]:
-    lines = __read_file(filename).split("\n")
-    return lines
+    return __read_without_trailing_newline(filename).split("\n")
 
 def get_input_grouped(filename: str = 'input.txt',
                       block_delimiter: str = " ") -> List:
     grouped = []
-    lines = __read_file(filename).split("\n\n")
+    lines = __read_without_trailing_newline(filename).split("\n\n")
     for line in lines:
         grouped.append(__input_blocks(line, block_delimiter))
     return grouped
@@ -18,5 +17,7 @@ def __input_blocks(lines, block_delimiter):
 
 def __read_file(filename: str):
     with open(filename) as f:
-        lines = f.read().rstrip("\n")
-    return lines
+        return f.read()
+
+def __read_without_trailing_newline(filename: str):
+    return __read_file(filename).rstrip("\n")

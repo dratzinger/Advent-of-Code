1fc228a0dfcdee934f578647959c30ccfa0ba80c
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2020-12-26 22:12:18 +0100

    day 10 - pt. 2: wip solution

--- day10/day10.py
+++ day10/day10.py
@@ -4,6 +4,7 @@ from typing import List
 from util.parser import get_input_lines
 
 
+# --- Part One ---
 def calculate_difference_distribution(data: List[str]):
     distribution = [0, 0, 0, 1]  # built-in adapter is always 3 higher than the highest adapter
     joltages = sorted([int(i) for i in data])
@@ -18,16 +19,40 @@ def calculate_difference_distribution(data: List[str]):
     return distribution
 
 
-# --- Part One ---
 def differences_multiplied(data):
     dist = calculate_difference_distribution(data)
     return dist[1] * dist[3]
 
 
 # --- Part Two ---
-def part_two(data):
+def calculate_possible_arrangements(data: List[str]):
+    numbers = sorted([int(i) for i in data])
+    shift1 = shift_list(numbers)
+    shift2 = shift_list(shift1)
+    shift3 = shift_list(shift2)
+    possible = [evaluate_options(i, j, k, l) for i, j, k, l in zip (numbers, shift1, shift2, shift3)]
+    return prod(possible)
+
+def prod(iterable):
+    from functools import reduce
+    import operator
+    return reduce(operator.mul, iterable, 1)
+
+def shift_list(l: list) -> list:
+    from collections import deque
+    q = deque(l)
+    q.rotate(-1)
+    return list(q)
+
+def evaluate_options(i, j, k, l):
     count = 0
+    count += (j - i <= 3)
+    count += (k - i <= 3)
+    count += (l - i <= 3)
     return count
+
+def part_two(data):
+    return calculate_possible_arrangements(data)
 
 
 def main():

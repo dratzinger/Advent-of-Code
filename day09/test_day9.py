eabd252d32a344c3340c91498183b726fdebcef7
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-04 21:28:06 +0100

    day 9: corrected test input

--- day09/test_day9.py
+++ day09/test_day9.py
@@ -5,7 +5,7 @@ from day9 import part_one, part_two
 
 class Test(unittest.TestCase):
     def test_part_1_known_input(self):
-        self.assertEqual(4, part_one(get_input_lines("test_input.txt")))
+        self.assertEqual(127, part_one(get_input_lines("test_input.txt"), 5))
 
     def test_part_2_known_input(self):
         self.assertEqual(0, part_two(get_input_lines("test_input.txt")))

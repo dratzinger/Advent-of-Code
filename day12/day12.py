3ea82e05e1db5a8f77487cbc22cd2305ac3cf8f1
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-05 19:09:56 +0100

    day 12 - pt. 1: implemented solution

--- day12/day12.py
+++ day12/day12.py
@@ -1,24 +1,14 @@
 # --- Day 12: Rain Risk ---
+from collections import namedtuple
 from util.parser import get_input_lines
 
 
 # --- Part One ---
-def part_one(data):
-    count = 0
-    return count
-
-
 class Ship(object):
     compass = {0: 'N', 90: 'E', 180: 'S', 270: 'W'}
 
     def __init__(self, bearing: int = 90) -> None:
         self.bearing = bearing
-
-    def turn_left(self, degrees: int):
-        self.turn(-degrees)
-
-    def turn_right(self, degrees: int):
-        self.turn(degrees)
 
     def turn(self, degrees: int):
         self.bearing = (self.bearing + degrees) % 360
@@ -27,6 +17,49 @@ class Ship(object):
         base = 90
         nearest = base * round(self.bearing / base)
         return Ship.compass.get(nearest)
+
+
+class State(object):
+    def __init__(self) -> None:
+        self.ship = Ship()
+        self.north = 0
+        self.east = 0
+
+    def move_north(self, value: int):
+        self.north += value
+
+    def move_east(self, value: int):
+        self.east += value
+
+    def execute(self, action: str, value: int):
+        if action == 'F':
+            action = self.ship.direction()
+
+        if action == 'N':
+            self.north += value
+        elif action == 'S':
+            self.north -= value
+        elif action == 'E':
+            self.east += value
+        elif action == 'W':
+            self.east -= value
+        elif action == 'R':
+            self.ship.turn(value)
+        elif action == 'L':
+            self.ship.turn(-value)
+        else:
+            raise RuntimeWarning("unknown instruction: "+action+str(value))
+
+    def calculate_distance(self):
+        return abs(self.north) + abs(self.east)
+
+
+def part_one(data):
+    navigation = State()
+    for instruction in data:
+        navigation.execute(instruction[:1], int(instruction[1:]))
+    return navigation.calculate_distance()
+
 
 # --- Part Two ---
 def part_two(data):

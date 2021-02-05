b476a73bfbb89bf4576cb9da4f1b6b79cc4c9f08
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-05 16:47:52 +0100

    day 12 - pt. 1: implemented ship state

--- day12/day12.py
+++ day12/day12.py
@@ -8,6 +8,26 @@ def part_one(data):
     return count
 
 
+class Ship(object):
+    compass = {0: 'N', 90: 'E', 180: 'S', 270: 'W'}
+
+    def __init__(self, bearing: int = 90) -> None:
+        self.bearing = bearing
+
+    def turn_left(self, degrees: int):
+        self.turn(-degrees)
+
+    def turn_right(self, degrees: int):
+        self.turn(degrees)
+
+    def turn(self, degrees: int):
+        self.bearing = (self.bearing + degrees) % 360
+
+    def direction(self) -> str:
+        base = 90
+        nearest = base * round(self.bearing / base)
+        return Ship.compass.get(nearest)
+
 # --- Part Two ---
 def part_two(data):
     count = 0
@@ -15,7 +35,7 @@ def part_two(data):
 
 
 def main():
-    data = get_input_lines()
+    data = get_input_lines("input.txt")
     print(part_one(data))
     print(part_two(data))
 

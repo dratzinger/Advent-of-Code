db3e536346152e14b13bac12ddb033c8e18dac5c
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-05 16:07:23 +0100

    parser refactoring
    - added type hints
    - used generator and list comprehension for memory efficiency

--- util/parser.py
+++ util/parser.py
@@ -6,23 +6,19 @@ def get_input_lines(filename: str = 'input.txt') -> List[str]:
     return __read_without_trailing_newline(filename).split("\n")
 
 
-def get_input_grouped(filename: str = 'input.txt',
-                      block_delimiter: str = " ") -> List:
-    grouped = []
-    lines = __read_without_trailing_newline(filename).split("\n\n")
-    for line in lines:
-        grouped.append(__input_blocks(line, block_delimiter))
-    return grouped
+def get_input_grouped(filename: str = 'input.txt', block_delimiter: str = " ") -> List[List[str]]:
+    lines = (__read_without_trailing_newline(filename).split("\n\n"))
+    return [__input_blocks(line, block_delimiter) for line in lines]
 
 
-def __input_blocks(lines, block_delimiter):
+def __input_blocks(lines, block_delimiter) -> List[str]:
     return re.split(block_delimiter + '|\n', lines)
 
 
-def __read_file(filename: str):
+def __read_file(filename: str) -> str:
     with open(filename) as f:
         return f.read()
 
 
-def __read_without_trailing_newline(filename: str):
+def __read_without_trailing_newline(filename: str) -> str:
     return __read_file(filename).rstrip("\n")

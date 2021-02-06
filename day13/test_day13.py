a1e2bb59e0362133e8ce355c13e12055634a1184
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-06 01:10:18 +0100

    day 13: added skeleton

---
+++ day13/test_day13.py
@@ -0,0 +1,15 @@
+import unittest
+from util.parser import get_input_lines
+from day13 import part_one, part_two
+
+
+class Test(unittest.TestCase):
+    def test_part_1_known_input(self):
+        self.assertEqual(295, part_one(get_input_lines("test_input.txt")))
+
+    def test_part_2_known_input(self):
+        self.assertEqual(0, part_two(get_input_lines("test_input.txt")))
+
+
+if __name__ == '__main__':
+    unittest.main()

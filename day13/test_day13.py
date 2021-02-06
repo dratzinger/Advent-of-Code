40a127af2a149d274de88b63cbeb9a7c55c6af4d
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-06 14:10:33 +0100

    day 13 - pt. 2: updated test

--- day13/test_day13.py
+++ day13/test_day13.py
@@ -8,7 +8,7 @@ class Test(unittest.TestCase):
         self.assertEqual(295, part_one(get_input_lines("test_input.txt")))
 
     def test_part_2_known_input(self):
-        self.assertEqual(0, part_two(get_input_lines("test_input.txt")))
+        self.assertEqual(1068781, part_two(get_input_lines("test_input.txt")))
 
 
 if __name__ == '__main__':

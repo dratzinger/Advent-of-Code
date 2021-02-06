6e9dfff22ea29c856ccdc701889d60f8032b4472
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-06 01:48:28 +0100

    day 13 - pt. 1: implemented solution

--- day13/day13.py
+++ day13/day13.py
@@ -1,10 +1,29 @@
 # --- Day 13: Shuttle Search ---
+from typing import List, Tuple
+from math import floor, ceil
 from util.parser import get_input_lines
 
+
 # --- Part One ---
+def closest_bus(timestamp: int, buses: List[int]) -> Tuple[int, int]:
+    times = departure_window(timestamp, buses)
+    wait = ((time-timestamp, bus) for time, bus in times if time >= timestamp)
+    return min(wait)
+
+
+def departure_window(timestamp: int, buses: List[int]):
+    for bus in buses:
+        factor = timestamp / bus
+        yield floor(factor) * bus, bus
+        yield ceil(factor) * bus, bus
+
+
 def part_one(data):
-    count = 0
-    return count
+    timestamp = int(data[0])
+    buses = (data[1].split(","))
+    in_service = [int(bus) for bus in buses if bus != 'x']
+    bus, wait = closest_bus(timestamp, in_service)
+    return bus * wait
 
 
 # --- Part Two ---

0f13158281220d6b6ee340c8f1ed85601aacffeb
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-12 21:16:35 +0100

    day 15 - pt. 1: added skeleton

---
+++ day15/day15.py
@@ -0,0 +1,24 @@
+# --- Day 15: Rambunctious Recitation ---
+from util.parser import get_input_lines
+
+
+# --- Part One ---
+def part_one(data):
+    count = 0
+    return count
+
+
+# --- Part Two ---
+def part_two(data):
+    count = 0
+    return count
+
+
+def main():
+    data = get_input_lines()
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == '__main__':
+    main()

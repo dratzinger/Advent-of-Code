d0d21c3148edfb50b5df1ede30a06e077fb07955
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-14 21:31:50 +0100

    day 18 - pt. 1: basic implementation

--- day18/day18.py
+++ day18/day18.py
@@ -1,12 +1,40 @@
 # --- Day 18: Operation Order ---
 from typing import Iterable
 
+from pyparsing import nestedExpr, Word, oneOf, nums, ZeroOrMore
+
 from util.parser import get_input_lines
 
-
 # --- Part One ---
+ops = {
+    '+': lambda x, y: x + y,
+    '*': lambda x, y: x * y,
+}
+
+
 def calculate(expression: str):
-    return expression
+    parsed = parse_nested(expression)
+
+    def evaluate_expression(exp: list):
+        left = exp.pop(0)
+        result = int(left) if left is not list else evaluate_expression(left)
+
+        while exp:
+            op = ops.get(exp.pop(0))
+            right = exp.pop(0)
+            right = int(right) if right is not list else evaluate_expression(right)
+            left = op(left, right)
+        return result
+
+    return evaluate_expression(parsed)
+
+
+
+def parse_nested(expression: str):
+    operators = oneOf("+ *")
+    nested_braces = nestedExpr('(', ')', )
+    combined = ZeroOrMore(Word(nums) | operators | nested_braces)
+    return combined.parseString(expression).asList()
 
 
 def part_one(data: Iterable) -> int:

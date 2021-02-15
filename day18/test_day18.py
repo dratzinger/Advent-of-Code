680e2f76713cf984db3b5f35edc58d41593b50c7
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-15 19:14:26 +0100

    day 18 - pt. 2: added test cases

--- day18/test_day18.py
+++ day18/test_day18.py
@@ -1,23 +1,41 @@
 import unittest
 
-from day18 import calculate, part_two
+from day18 import calculate, calculate_prioritized
+
+EXPRESSION_0 = "1 + (2 * 3) + (4 * (5 + 6))"
+EXPRESSION_1 = "2 * 3 + (4 * 5)"
+EXPRESSION_2 = "5 + (8 * 3 + 9 + 3 * 4 * 3)"
+EXPRESSION_3 = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
+EXPRESSION_4 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
 
 
 class Test(unittest.TestCase):
     def test_part_1_known_input_1(self):
-        self.assertEqual(26, calculate("2 * 3 + (4 * 5)"))
+        self.assertEqual(26, calculate(EXPRESSION_1))
 
     def test_part_1_known_input_2(self):
-        self.assertEqual(437, calculate("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+        self.assertEqual(437, calculate(EXPRESSION_2))
 
     def test_part_1_known_input_3(self):
-        self.assertEqual(12240, calculate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+        self.assertEqual(12240, calculate(EXPRESSION_3))
 
     def test_part_1_known_input_4(self):
-        self.assertEqual(13632, calculate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+        self.assertEqual(13632, calculate(EXPRESSION_4))
 
     def test_part_2_known_input(self):
-        self.assertEqual(0, part_two([]))
+        self.assertEqual(51, calculate_prioritized(EXPRESSION_0))
+
+    def test_part_2_known_input_1(self):
+        self.assertEqual(46, calculate_prioritized(EXPRESSION_1))
+
+    def test_part_2_known_input_2(self):
+        self.assertEqual(1445, calculate_prioritized(EXPRESSION_2))
+
+    def test_part_2_known_input_3(self):
+        self.assertEqual(669060, calculate_prioritized(EXPRESSION_3))
+
+    def test_part_2_known_input_4(self):
+        self.assertEqual(23340, calculate_prioritized(EXPRESSION_4))
 
 
 if __name__ == '__main__':

517758ab350e8398d62f90b1a3739672cf6d3df1
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-02-28 19:07:20 +0100

    improved day template

--- template/dayX.py
+++ template/dayX.py
@@ -1,20 +1,21 @@
 # --- Day X: Template ---
+from typing import Iterable
+
 from util.parser import get_input_lines
 
+
 # --- Part One ---
-def part_one(data):
-    count = 0
-    return count
+def part_one(data: Iterable) -> int:
+    return 0
 
 
 # --- Part Two ---
-def part_two(data):
-    count = 0
-    return count
+def part_two(data: Iterable) -> int:
+    return 0
 
 
 def main():
-    data = get_input_lines()
+    data = (get_input_lines())
     print(part_one(data))
     print(part_two(data))
 
--- template/test_dayX.py
+++ template/test_dayX.py
@@ -1,14 +1,16 @@
 import unittest
-from util.parser import get_input_lines
+
 from dayX import part_one, part_two
+
+INPUT_0 = ""
 
 
 class Test(unittest.TestCase):
-    def test_part_1_known_input(self):
-        self.assertEqual(4, part_one(get_input_lines("test_input.txt")))
+    def test_part_1_known_input_1(self):
+        self.assertEqual(0, part_one(INPUT_0))
 
     def test_part_2_known_input(self):
-        self.assertEqual(0, part_two(get_input_lines("test_input.txt")))
+        self.assertEqual(0, part_two(INPUT_0))
 
 
 if __name__ == '__main__':

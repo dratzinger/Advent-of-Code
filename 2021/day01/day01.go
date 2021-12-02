1781ee9d8d2e4662f6cdf5e68e2e54541095ab1c
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-02 21:43:00 +0100

    day 01 - pt. 1: solution implemented

--- 2021/day01/day01.go
+++ 2021/day01/day01.go
@@ -11,6 +11,13 @@ func parseInput(file string) []int {
 	return []int{}
 }
 
-func Part1(input []int) int {
-	return 0
+func Part1(input []int) (count int) {
+	prev := 0
+	for i, val := range input {
+		if i > 0 && val > prev {
+			count++
+		}
+		prev = val
+	}
+	return count
 }

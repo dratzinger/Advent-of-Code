b403116612c8679c1f979b9192a37a701618efd6
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-02 21:52:47 +0100

    day 01: file parsing added

--- 2021/day01/day01.go
+++ 2021/day01/day01.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	in := parseInput("input.txt")
 	fmt.Println("Day 1 - Pt. 1:", Part1(in))
 }
 
-func parseInput(file string) []int {
-	return []int{}
+func parseInput(filename string) []int {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var line int
+	var nums []int
+
+	for {
+		// give a pattern to scan
+		_, err := fmt.Fscanf(file, "%d\n", &line)
+
+		if err != nil {
+			if err == io.EOF {
+				// stop reading the file
+				break
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		nums = append(nums, line)
+	}
+	return nums
 }
 
 func Part1(input []int) (count int) {

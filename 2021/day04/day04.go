da7c32174b7e1c49c83b1f1da4ec949af0d68ca2
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-04 14:23:32 +0100

    day 04 - pt. 1: stubbs

--- 2021/day04/day04.go
+++ 2021/day04/day04.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	parse "github.com/dratzinger/Advent-of-Code/2021/util"
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	in := parse.StrLines("input.txt")
+	content := parse.Read("input.txt")
+	in := parse.Blocks(content, "\n\n")
 	fmt.Println("Day 2 - Pt. 1:", Part1(in))
 	fmt.Println("Day 2 - Pt. 2:", Part2(in))
 }
 
-func Part1(input []string) (count int) {
+func Part1(input []string) (score int) {
+	nums := strings.Split(input[0], ",")
+	draws := parse.IntSlice(nums)
+	boards := makeBoards(input[1:])
+	checked := makeChecked(len(boards))
+
+	for _, call := range draws {
+		winner := playBingo(call, &boards, &checked)
+		if winner != -1 {
+			score = mat.Mul(boards[winner], checked[winner])
+		}
+	}
 	return
 }
 
 func Part2(input []string) (count int) {
 	return
+}
+
+func makeBoards(data []string) (boards []mat.Matrix) {
+	for _, board := range data {
+		boards = append(boards, parse.Matrix(board, 5, 5))
+	}
+	return boards
+}
+
+func makeChecked(count int) (checked []mat.Matrix) {
+	for i := 0; i < count; i++ {
+		var zeros [25]float64
+		checked = append(checked, mat.NewDense(5, 5, zeros[:]))
+	}
+	return checked
+}
+
+func playBingo(call int, boards, checked *[]mat.Matrix) int {
+	return 0
+}
+
+func sum(unchecked mat.Matrix) int {
+	return 0
 }

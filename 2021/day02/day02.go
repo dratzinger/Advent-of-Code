03f3e934c1ba96f8df4fd83a0579a3f195b0c416
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-04 01:07:44 +0100

    day 02 - pt. 1: solution implemented

--- 2021/day02/day02.go
+++ 2021/day02/day02.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/dratzinger/Advent-of-Code/2021/util"
+	"log"
+
+	parse "github.com/dratzinger/Advent-of-Code/2021/util"
 )
 
 func main() {
@@ -11,8 +13,29 @@ func main() {
 	fmt.Println("Day 2 - Pt. 2:", Part2(in))
 }
 
-func Part1(input []string) (count int) {
-	return
+func Part1(input []string) int {
+	xPos, yPos := 0, 0
+
+	var direction string
+	var value int
+
+	for _, line := range input {
+		_, err := fmt.Sscanf(line, "%s %d", &direction, &value)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch direction {
+		case "forward":
+			xPos += value
+		case "down":
+			yPos += value
+		case "up":
+			yPos -= value
+		}
+	}
+	return xPos * yPos
 }
 
 func Part2(input []string) (count int) {

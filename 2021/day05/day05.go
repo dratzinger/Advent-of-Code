25d141183e146e55e9f8f0a4b7d7252858cfe2ce
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-05 10:27:38 +0100

    day 05 added

---
+++ 2021/day05/day05.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	parse "github.com/dratzinger/Advent-of-Code/2021/util"
+)
+
+func main() {
+	in := parse.StrLines("input.txt")
+	fmt.Println("Day 2 - Pt. 1:", Part1(in))
+	fmt.Println("Day 2 - Pt. 2:", Part2(in))
+}
+
+func Part1(input []string) (score int) {
+	return
+}
+
+func Part2(input []string) (count int) {
+	return
+}
---
+++ 2021/day05/day05_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+var input = []string{
+	"0,9 -> 5,9",
+	"8,0 -> 0,8",
+	"9,4 -> 3,4",
+	"2,2 -> 2,1",
+	"7,0 -> 7,4",
+	"6,4 -> 2,0",
+	"0,9 -> 2,9",
+	"3,4 -> 1,4",
+	"0,0 -> 8,8",
+	"5,5 -> 8,2",
+}
+
+func TestPart1(t *testing.T) {
+	result := Part1(input)
+	reference := 5
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(input)
+	reference := -1
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}

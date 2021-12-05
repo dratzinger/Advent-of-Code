0b96e8ee93aaa4e269149129c5fbbc0f40132eb1
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-05 16:19:45 +0100

    day 05: unused functions removed

--- 2021/day05/day05.go
+++ 2021/day05/day05.go
@@ -70,26 +70,6 @@ func initChart(xDim, yDim int) [][]int {
 	return chart
 }
 
-func orthogonalLine(chart [][]int, line line) [][]int {
-	xMin := min(line.start.x, line.end.x)
-	xMax := max(line.start.x, line.end.x)
-
-	yMin := min(line.start.y, line.end.y)
-	yMax := max(line.start.y, line.end.y)
-
-	for y := range chart {
-		for x := range chart {
-			horizontal := x == line.start.x && (yMin <= y && y <= yMax)
-			vertical := y == line.start.y && (xMin <= x && x <= xMax)
-
-			if vertical || horizontal {
-				chart[y][x]++
-			}
-		}
-	}
-	return chart
-}
-
 func drawLine(chart [][]int, line line) [][]int {
 
 	setPixel := func(x, y int) {
@@ -146,16 +126,6 @@ func Abs(x int) int {
 		return -x
 	}
 	return x
-}
-
-func min(vals ...int) int {
-	min := vals[0]
-	for _, v := range vals {
-		if v < min {
-			min = v
-		}
-	}
-	return min
 }
 
 func max(vals ...int) int {

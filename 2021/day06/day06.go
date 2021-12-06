75091f614aa0ed8ecaa27a8bd4972104acfccd6f
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2021-12-06 12:58:55 +0100

    day 06 added

---
+++ 2021/day06/day06.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
+)
+
+func main() {
+	str := parse.Read("input.txt")
+	in := strings.Split(str, ",")
+	fmt.Println("Day 6 - Pt. 1:", Part1(in))
+	fmt.Println("Day 6 - Pt. 2:", Part2(in))
+}
+
+func Part1(input []string) int {
+	return 0
+}
+
+func Part2(input []string) int {
+	return 0
+}
---
+++ 2021/day06/day06_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+const example = "3,4,3,1,2"
+
+var input = strings.Split(example, ",")
+
+func TestPart1(t *testing.T) {
+	result := Part1(input)
+	reference := 5934
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(input)
+	reference := -1
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}

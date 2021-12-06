577f3975e5c5776080d6a24e8c5d7bf62d8310f3
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2021-12-06 13:16:07 +0100

    day 06 - pt. 1: solution implemented

--- 2021/day06/day06.go
+++ 2021/day06/day06.go
@@ -9,15 +9,36 @@ import (
 
 func main() {
 	str := parse.Read("input.txt")
-	in := strings.Split(str, ",")
+	data := strings.ReplaceAll(str, "\n", "")
+	in := strings.Split(data, ",")
 	fmt.Println("Day 6 - Pt. 1:", Part1(in))
 	fmt.Println("Day 6 - Pt. 2:", Part2(in))
 }
 
 func Part1(input []string) int {
-	return 0
+	population := parse.IntSlice(input)
+	days := 80
+	return len(simulate(population, days, 6))
 }
 
 func Part2(input []string) int {
 	return 0
+}
+
+func simulate(population []int, steps, cycleTime int) []int {
+	for i := 0; i < steps; i++ {
+		population = simpleSim(population, cycleTime)
+	}
+	return population
+}
+
+func simpleSim(population []int, cycleTime int) (result []int) {
+	for _, val := range population {
+		if val == 0 {
+			result = append(result, cycleTime, cycleTime+2)
+		} else {
+			result = append(result, val-1)
+		}
+	}
+	return result
 }

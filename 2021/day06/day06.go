ed5374c75c0630b1a471614cd8d1287349835a45
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2021-12-06 19:22:15 +0100

    day 06 - pt. 2: solution implemented

--- 2021/day06/day06.go
+++ 2021/day06/day06.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/dratzinger/Advent-of-Code/2021/util/integers"
 	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
 )
 
@@ -22,7 +23,8 @@ func Part1(input []string) int {
 }
 
 func Part2(input []string) int {
-	return 0
+	population := parse.IntSlice(input)
+	return binnedSim(population, 256)
 }
 
 func simulate(population []int, steps, cycleTime int) []int {
@@ -41,4 +43,26 @@ func simpleSim(population []int, cycleTime int) (result []int) {
 		}
 	}
 	return result
+}
+
+func binnedSim(population []int, days int) int {
+	bins := [9]int{}
+	for _, val := range population {
+		bins[val]++
+	}
+	for day := 0; day < days; day++ {
+		var spawning int
+		for daysLeft, count := range bins {
+			if daysLeft == 0 {
+				spawning = count
+			} else {
+				bins[daysLeft-1] = count
+			}
+			if daysLeft == 8 {
+				bins[6] += spawning
+				bins[8] = spawning
+			}
+		}
+	}
+	return integers.Sum(bins[:]...)
 }

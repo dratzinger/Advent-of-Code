d8550e7ac676327408ea3b88edcfc5295a3c6461
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-07 21:58:26 +0100

    day 07 - pt. 2: solution implemented

--- 2021/day07/day07.go
+++ 2021/day07/day07.go
@@ -19,12 +19,16 @@ func main() {
 func Part1(input []string) int {
 	positions := parse.IntSlice(input)
 	mean := int(integers.Mean(positions...))
-	return bruteMinimize(positions, mean, .25)
+	return bruteMinimize(positions, mean, .25, differences)
 }
 
 func Part2(input []string) (count int) {
-	return
+	positions := parse.IntSlice(input)
+	mean := int(integers.Mean(positions...))
+	return bruteMinimize(positions, mean, .25, triangularDifferences)
 }
+
+type difference func([]int, int) int
 
 func differences(positions []int, val int) (sum int) {
 	for _, v := range positions {
@@ -33,14 +37,27 @@ func differences(positions []int, val int) (sum int) {
 	return sum
 }
 
-// try aligning to a value plus minus a percentage of the data range
-// then return the minimum
-func bruteMinimize(positions []int, align int, percentage float64) int {
+// cost increases with each step (n-th triangular number)
+func triangularDifferences(positions []int, val int) (sum int) {
+	triangular := func(n int) int {
+		return n * (n + 1) / 2
+	}
+
+	for _, v := range positions {
+		diff := integers.AbsDiff(v, val)
+		sum += triangular(diff)
+	}
+	return sum
+}
+
+// try aligning the positions to a value plus minus a percentage of the data range
+// then return the minimum of those tries
+func bruteMinimize(positions []int, align int, percentage float64, fn difference) int {
 	dataRange := float64(integers.Range(positions...))
 	offset := int(dataRange * percentage)
 	cost := []int{}
 	for val := align - offset; val < align+offset; val++ {
-		cost = append(cost, differences(positions, val))
+		cost = append(cost, fn(positions, val))
 	}
 	return integers.Min(cost...)
 }

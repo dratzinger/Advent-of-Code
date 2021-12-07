2bb5544120c47f9a628e4892c2855648478828eb
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-07 21:24:28 +0100

    day 07 - pt. 1: solution implemented

--- 2021/day07/day07.go
+++ 2021/day07/day07.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/dratzinger/Advent-of-Code/2021/util/integers"
 	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
 )
 
 func main() {
-	in := parse.StrLines("input.txt")
+	str := parse.Read("input.txt")
+	data := strings.ReplaceAll(str, "\n", "")
+	in := strings.Split(data, ",")
 	fmt.Println("Day 7 - Pt. 1:", Part1(in))
 	fmt.Println("Day 7 - Pt. 2:", Part2(in))
 }
 
-func Part1(input []string) (count int) {
-	return
+func Part1(input []string) int {
+	positions := parse.IntSlice(input)
+	mean := int(integers.Mean(positions...))
+	return bruteMinimize(positions, mean, .25)
 }
 
 func Part2(input []string) (count int) {
 	return
+}
+
+func differences(positions []int, val int) (sum int) {
+	for _, v := range positions {
+		sum += integers.AbsDiff(v, val)
+	}
+	return sum
+}
+
+// try aligning to a value plus minus a percentage of the data range
+// then return the minimum
+func bruteMinimize(positions []int, align int, percentage float64) int {
+	dataRange := float64(integers.Range(positions...))
+	offset := int(dataRange * percentage)
+	cost := []int{}
+	for val := align - offset; val < align+offset; val++ {
+		cost = append(cost, differences(positions, val))
+	}
+	return integers.Min(cost...)
 }
--- 2021/util/integers/integers.go
+++ 2021/util/integers/integers.go
@@ -1,5 +1,7 @@
 package integers
 
+import "sort"
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
@@ -7,24 +9,44 @@ func Abs(x int) int {
 	return x
 }
 
-func Max(vals ...int) int {
-	max := vals[0]
+func AbsDiff(a, b int) int {
+	return Abs(a - b)
+}
+
+func Extrema(vals ...int) (min, max int) {
+	min = vals[0]
+	max = vals[0]
 	for _, v := range vals {
 		if v > max {
 			max = v
+		} else if v < min {
+			min = v
 		}
 	}
+	return min, max
+}
+
+func Even(x int) bool {
+	return x%2 == 0
+}
+
+func Odd(x int) bool {
+	return !Even(x)
+}
+
+func Max(vals ...int) int {
+	_, max := Extrema(vals...)
 	return max
 }
 
 func Min(vals ...int) int {
-	min := vals[0]
-	for _, v := range vals {
-		if v < min {
-			min = v
-		}
-	}
+	min, _ := Extrema(vals...)
 	return min
+}
+
+func Range(vals ...int) int {
+	min, max := Extrema(vals...)
+	return max - min
 }
 
 func Sum(vals ...int) (sum int) {
@@ -32,4 +54,21 @@ func Sum(vals ...int) (sum int) {
 		sum += val
 	}
 	return sum
+}
+
+func Mean(vals ...int) float64 {
+	sum := Sum(vals...)
+	n := len(vals)
+	return float64(sum) / float64(n)
+}
+
+func Median(vals ...int) float64 {
+	sorted := sort.IntSlice(vals)
+	length := len(vals)
+	middle := length / 2
+
+	if Odd(length) {
+		return float64(sorted[middle])
+	}
+	return Mean(sorted[middle-1] + sorted[middle])
 }

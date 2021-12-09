9e1197340c58d42bee1180bae5ea0dbbfd81ccd8
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-10 00:44:27 +0100

    day 09 - pt. 1: solution implemented

--- 2021/day09/day09.go
+++ 2021/day09/day09.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/dratzinger/Advent-of-Code/2021/util/integers"
 	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
 )
 
@@ -13,9 +14,61 @@ func main() {
 }
 
 func Part1(input []string) (count int) {
-	return
+	heightMap := makeHeightmap(input)
+	lowPoints := findLowPoints(heightMap)
+	risk := integers.Sum(lowPoints...) + len(lowPoints)
+	return risk
 }
 
 func Part2(input []string) (count int) {
 	return
+}
+
+func makeHeightmap(input []string) (heights [][]int) {
+	for _, line := range input {
+		if values := []int{}; line != "" {
+			for _, v := range line {
+				point := string(v)
+				values = append(values, parse.ToInt(point))
+			}
+			heights = append(heights, values)
+		}
+	}
+	return heights
+}
+
+func findLowPoints(heights [][]int) (lowPoints []int) {
+	var directions = func(x, y int) (directions [][]int) {
+		if x != 0 {
+			directions = append(directions, []int{-1, 0})
+		}
+		if x != len(heights[0])-1 {
+			directions = append(directions, []int{1, 0})
+		}
+		if y != 0 {
+			directions = append(directions, []int{0, -1})
+		}
+		if y != len(heights)-1 {
+			directions = append(directions, []int{0, 1})
+		}
+		return directions
+	}
+
+	for y, row := range heights {
+		for x, height := range row {
+			var isLowPoint = func(heights [][]int, x, y int) bool {
+				for _, direction := range directions(x, y) {
+					if heights[y+direction[1]][x+direction[0]] <= height {
+						return false
+					}
+				}
+				return true
+			}
+
+			if isLowPoint(heights, x, y) {
+				lowPoints = append(lowPoints, heights[y][x])
+			}
+		}
+	}
+	return lowPoints
 }

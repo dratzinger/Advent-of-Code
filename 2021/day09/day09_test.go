ab487bab4bc39b7cc18cd61af70483de7cafa760
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-09 20:42:19 +0100

    day 09 added

---
+++ 2021/day09/day09_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+var input = []string{
+	"2199943210",
+	"3987894921",
+	"9856789892",
+	"8767896789",
+	"9899965678",
+}
+
+func TestPart1(t *testing.T) {
+	result := Part1(input)
+	reference := 15
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(input)
+	reference := -1
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}

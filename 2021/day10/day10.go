f4b4ffb80b601c82a71b82d664b7f578abb8bf70
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2021-12-10 18:31:06 +0100

    day 10 added

---
+++ 2021/day10/day10.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
+)
+
+func main() {
+	in := parse.StrLines("input.txt")
+	fmt.Println("Day 10 - Pt. 1:", Part1(in))
+	fmt.Println("Day 10 - Pt. 2:", Part2(in))
+}
+
+func Part1(input []string) (count int) {
+	return
+}
+
+func Part2(input []string) (count int) {
+	return
+}

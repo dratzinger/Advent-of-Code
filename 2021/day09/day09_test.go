bca071b573ea849829078e1c733e98ecd026eaa6
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2021-12-10 18:19:33 +0100

    day 09 - pt. 2: description and test added

--- 2021/day09/day09_test.go
+++ 2021/day09/day09_test.go
@@ -20,7 +20,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	result := Part2(input)
-	reference := -1
+	reference := 1134
 	if result != reference {
 		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
 	}

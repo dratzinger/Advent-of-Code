e55b827ec1221f43030a9259854ab6eb146c84c8
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-13 23:14:15 +0100

    day 09 - pt. 2: solution implemented

--- 2021/day09/day09.go
+++ 2021/day09/day09.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
-	"github.com/dratzinger/Advent-of-Code/2021/util/integers"
 	"github.com/dratzinger/Advent-of-Code/2021/util/parse"
 )
+
+type point struct {
+	x, y int
+}
 
 func main() {
 	in := parse.StrLines("input.txt")
@@ -13,16 +17,39 @@ func main() {
 	fmt.Println("Day 9 - Pt. 2:", Part2(in))
 }
 
-func Part1(input []string) (count int) {
-	heightMap := makeHeightmap(input)
-	lowPoints := findLowPoints(heightMap)
-	risk := integers.Sum(lowPoints...) + len(lowPoints)
+func Part1(input []string) (risk int) {
+	heightMap, lowPoints := crunch(input)
+	for _, p := range lowPoints {
+		risk += 1 + heightMap[p.y][p.x]
+	}
 	return risk
 }
 
 func Part2(input []string) (count int) {
-	return
+	heightMap, lowPoints := crunch(input)
+
+	basins := []int{}
+	for _, v := range lowPoints {
+		sum := FloodSum(heightMap, v.x, v.y)
+		basins = append(basins, sum)
+	}
+	// sort the basins
+	sort.IntSlice(basins).Sort()
+	topThree := basins[len(basins)-3:]
+
+	for _, v := range topThree {
+		count += v
+	}
+	return count
 }
+
+func crunch(input []string) (heightMap [][]int, lowPoints []point) {
+	heightMap = makeHeightmap(input)
+	lowPoints = findLowPoints(heightMap)
+	return heightMap, lowPoints
+}
+
+var directions func(int, int) [][]int
 
 func makeHeightmap(input []string) (heights [][]int) {
 	for _, line := range input {
@@ -34,6 +61,7 @@ func makeHeightmap(input []string) (heights [][]int) {
 			heights = append(heights, values)
 		}
 	}
+	directions = makeDirectionFn(len(heights[0])-1, len(heights)-1)
 	return heights
 }
 
@@ -56,9 +84,7 @@ func makeDirectionFn(maxX, maxY int) func(int, int) [][]int {
 	return dirFn
 }
 
-func findLowPoints(heights [][]int) (lowPoints []int) {
-	directions := makeDirectionFn(len(heights[0])-1, len(heights)-1)
-
+func findLowPoints(heights [][]int) (lowPoints []point) {
 	for y, row := range heights {
 		for x, height := range row {
 			var isLowPoint = func(heights [][]int, x, y int) bool {
@@ -71,9 +97,20 @@ func findLowPoints(heights [][]int) (lowPoints []int) {
 			}
 
 			if isLowPoint(heights, x, y) {
-				lowPoints = append(lowPoints, heights[y][x])
+				lowPoints = append(lowPoints, point{x, y})
 			}
 		}
 	}
 	return lowPoints
+}
+
+func FloodSum(heights [][]int, x, y int) (sum int) {
+	sum += heights[y][x]
+	heights[y][x] = 0
+	for _, direction := range directions(x, y) {
+		if heights[y+direction[1]][x+direction[0]] != 9 {
+			sum += FloodSum(heights, x+direction[0], y+direction[1])
+		}
+	}
+	return sum
 }

c33e8bab5067eef9d9f8611f91b0d26782aac7f5
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2021-12-19 23:10:57 +0100

    day 08 - pt. 2: description and test added

--- 2021/day08/day08_test.go
+++ 2021/day08/day08_test.go
@@ -15,6 +15,10 @@ var input = []string{
 	"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
 }
 
+var small = []string{
+	"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+}
+
 func TestPart1(t *testing.T) {
 	result := Part1(input)
 	reference := 26
@@ -23,9 +27,17 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-func TestPart2(t *testing.T) {
+func TestPart2_small(t *testing.T) {
+	result := Part2(small)
+	reference := 5353
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}
+
+func TestPart2_large(t *testing.T) {
 	result := Part2(input)
-	reference := -1
+	reference := 61229
 	if result != reference {
 		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
 	}

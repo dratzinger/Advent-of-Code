40bf6975dc437c787b034abcee137aa5af4705c8
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2022-12-03 11:55:45 +0100

    day 03 - pt. 1: description and test added

---
+++ 2022/day03/day03_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+var input = []string{
+	"vJrwpWtwJgWrhcsFMMfFFhFp",
+	"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+	"PmmdzqPrVvPwwTWBwg",
+	"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+	"ttgJtRGJQctTZtZT",
+	"CrZsJsPPZsGzwwsLwLmpwMDw",
+}
+
+func TestPart1(t *testing.T) {
+	result := Part1(input)
+	reference := 157
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(input)
+	reference := -1
+	if result != reference {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", result, reference)
+	}
+}

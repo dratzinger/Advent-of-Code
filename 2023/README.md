a90fb5a4430b8482b4a8ce43b33ec1721e221b10
Daniel Ratzinger <daniel.ratzinger@student.tuwien.ac.at>
2023-12-02 18:04:17 +0100

    day 01 - pt. 2 solved

--- 2023/README.md
+++ 2023/README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -57,9 +57,9 @@ npm start <day>
 
 ```
 Day 01
-Time part 1: 1.659ms
-Time part 2: -
-Both parts: 1.659ms
+Time part 1: 53.23ms
+Time part 2: 62.621ms
+Both parts: 115.851ms
 ```
 
 ```
@@ -231,8 +231,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 1.659ms
+Total stars: 2/50
+Total time: 115.851ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day01/index.ts
+++ 2023/src/day01/index.ts
@@ -4,27 +4,45 @@ const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
   const lines = input.split('\n');
-  const numDigit = /\d/g;
-  const values = lines
-    .map((line) => {
-      const digits = Array.of(...line.matchAll(numDigit));
-      const first = digits[0] ?? '';
-      const last = digits[digits.length - 1] ?? first;
-      return Number.parseInt(`${first}${last}`);
-    })
-    .filter(Number.isInteger);
-
-  const sum = values.reduce((acc, val) => (acc += val), 0);
-
-  return sum;
+  return crunch(lines, /(\d)/g);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const lines = input.split('\n');
+  return crunch(
+    lines,
+    /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g,
+  );
+};
 
-  return;
+const crunch = (lines: string[], pattern: RegExp) =>
+  lines
+    .map((line) => Array.from(line.matchAll(pattern), (match) => match[1]))
+    .map((digits) => {
+      console.log(digits);
+      const first = String(digits[0] ?? '');
+      const last = String(digits[digits.length - 1] ?? first);
+      const d1 = lookup[first] ?? first;
+      const d2 = lookup[last] ?? last;
+      const valStr = `${d1}${d2}`;
+      const val = Number.parseInt(valStr);
+      return val;
+    })
+    .filter(Number.isInteger)
+    .reduce((acc, val) => (acc += val), 0);
+
+const lookup: { [id: string]: string } = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
 };
 
 run({
@@ -44,10 +62,18 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        `,
+        expected: 281,
+      },
     ],
     solution: part2,
   },

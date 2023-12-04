be1d57f7fa8bb5639700d57ec5d4c4c0d9cd9ac2
Daniel Ratzinger <daniel.ratzinger@powerbot-trading.com>
2023-12-04 14:25:29 +0100

    day 04 - pt. 2 solved

--- 2023/src/day04/index.ts
+++ 2023/src/day04/index.ts
@@ -10,35 +10,58 @@ const part1 = (rawInput: string) => {
     .map((c) => c.map((nums) => nums.trim().split(/\ +/)));
   const points = raw.flatMap(([winning, nums]) => {
     const winningSet = new Set(winning);
-    console.log({ nums, winningSet });
     const i = nums
       .map((n) => winningSet.has(n))
       .filter(Boolean)
-      .map((_, i) => (i === 0 ? 1 : 2));
+      .map((_, i): number => (i === 0 ? 1 : 2));
     return i.length < 1 ? 0 : i.reduce((p, multiplier) => p * multiplier, 1);
   });
-  console.log({ points });
   return points.reduce((sum, p) => (sum += p));
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const lines = input.split('\n');
+  const raw = lines
+    .map((l) => l.split(': ')[1].split(' | '))
+    .map((c) => c.map((nums) => nums.trim().split(/\ +/)));
+  const points = raw.flatMap(([winning, nums], id) => {
+    const winningSet = new Set(winning);
+    const wins = nums
+      .map((n) => winningSet.has(n))
+      .reduce((sum, won) => (won ? sum + 1 : sum), 0);
+    return { id, wins };
+  });
 
-  return;
+  const cards = points.map((i) => 1);
+  const stack = [...points];
+
+  let current;
+  while ((current = stack.pop())) {
+    let id = current.id;
+    for (let i = current.wins; i > 0; i--) {
+      id++;
+      cards[id] += 1;
+      stack.push(points[id]);
+    }
+  }
+  return cards.reduce((sum, count) => (sum += count));
 };
+
+const testInput = `
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`;
 
 run({
   part1: {
     tests: [
       {
-        input: `
-        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-        `,
+        input: testInput,
         expected: 13,
       },
     ],
@@ -46,10 +69,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: testInput,
+        expected: 30,
+      },
     ],
     solution: part2,
   },

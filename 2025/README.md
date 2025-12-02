b03d7324a2244a9877f380dbe3a76c13bb4e3c4d
Daniel Ratzinger <26470120+dratzinger@users.noreply.github.com>
2025-12-02 01:45:08 +0100

    day 01 - pt. 2 dumb solution

--- 2025/README.md
+++ 2025/README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -56,9 +56,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.361ms
-Time part 2: -
-Both parts: 0.361ms
+Time part 1: 0.332ms
+Time part 2: 5.994ms
+Both parts: 6.326ms
 ```
 
 ```
@@ -139,8 +139,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 0.361ms
+Total stars: 2/24
+Total time: 6.326ms
 ```
 
 <!--/RESULTS-->
--- 2025/src/day01/index.ts
+++ 2025/src/day01/index.ts
@@ -15,37 +15,38 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  return;
+  let dial = 50;
+  return input.split("\n").reduce((count, current) => {
+    const left = current.slice(0, 1) === "L";
+    const amt = Number(current.slice(1));
+    for (let i = amt; i > 0; i--) {
+      dial = (dial + (left ? -1 : 1)) % 100;
+      if (dial === 0) count++;
+    }
+    return count;
+  }, 0);
 };
+
+const input = `
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+`;
 
 run({
   part1: {
-    tests: [
-      {
-        input: `
-        L68
-        L30
-        R48
-        L5
-        R60
-        L55
-        L1
-        L99
-        R14
-        L82
-        `,
-        expected: "3",
-      },
-    ],
+    tests: [{ input, expected: 3 }],
     solution: part1,
   },
   part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
+    tests: [{ input, expected: 6 }],
     solution: part2,
   },
   trimTestInputs: true,

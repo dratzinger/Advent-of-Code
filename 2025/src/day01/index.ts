6d2953a94bfe62055374a3546352041aa338bfeb
Daniel Ratzinger <26470120+dratzinger@users.noreply.github.com>
2025-12-02 01:04:56 +0100

    day 01 - pt. 1 solved

---
+++ 2025/src/day01/index.ts
@@ -0,0 +1,55 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let dial = 50;
+  let count = 0;
+  input.split("\n").forEach((current) => {
+    const left = current.slice(0, 1) === "L";
+    const amt = Number(current.slice(1));
+    dial = (left ? dial - amt : dial + amt) % 100;
+    count = count + (dial === 0 ? 1 : 0);
+  });
+  return String(count);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+        `,
+        expected: "3",
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});

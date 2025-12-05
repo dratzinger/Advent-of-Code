02a19303e2f80a197aad2e5c330bdc12c4ed1d63
Daniel Ratzinger <26470120+dratzinger@users.noreply.github.com>
2025-12-05 20:22:15 +0100

    day 03 - pt. 2 add test, wip

--- 2025/src/day03/index.ts
+++ 2025/src/day03/index.ts
@@ -18,33 +18,37 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  return;
+  const joltages = parseInput(rawInput);
+  const sums = joltages.map((j) =>
+    Array.from(j)
+      .map((val, idx) => ({ val, idx }))
+      .sort((a, b) => a.val.localeCompare(b.val))
+      .slice(-12)
+      .sort((a, b) => a.idx - b.idx)
+      .map((el) => el.val)
+      .reduce((sum, char) => sum + char),
+  );
+  const converted = sums.map(Number);
+  const len = sums.map((s) => s.length);
+  console.log({ sums, len, converted });
+  return converted.reduce((acc: number, j: number) => acc + j);
 };
 
-run({
-  part1: {
-    tests: [
-      {
-        input: `
+const input = `
         987654321111111
         811111111111119
         234234234234278
         818181911112111
-        `,
-        expected: 357,
-      },
-    ],
+        `;
+run({
+  part1: {
+    tests: [{ input, expected: 357 }],
     solution: part1,
   },
   part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
+    tests: [{ input, expected: 3121910778619 }],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 });

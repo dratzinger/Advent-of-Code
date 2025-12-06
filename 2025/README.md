30bd204c4513b4eb71c540ae51deef38c83e879f
Daniel Ratzinger <26470120+dratzinger@users.noreply.github.com>
2025-12-06 03:19:22 +0100

    day 04 - pt. 2 solved

--- 2025/README.md
+++ 2025/README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
-[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -77,9 +77,9 @@ Both parts: 0.618ms
 
 ```
 Day 04
-Time part 1: 3.72ms
-Time part 2: -
-Both parts: 3.72ms
+Time part 1: 6.065ms
+Time part 2: 111.641ms
+Both parts: 117.706ms
 ```
 
 ```
@@ -139,8 +139,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 312.706ms
+Total stars: 7/50
+Total time: 426.691ms
 ```
 
 <!--/RESULTS-->
--- 2025/src/day04/index.ts
+++ 2025/src/day04/index.ts
@@ -16,16 +16,13 @@ const getNeighbors = (input: Matrix<string>, y: number, x: number) => [
   input[y + 1]?.[x + 1],
 ];
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const mapAccessible = (input: Matrix<string>, threshold = 4, roll = "@") => {
   const rows = input.length;
   const cols = input[0].length;
   const rollcount: Matrix<number> = Array(rows)
     .fill(null)
     .map((_) => Array(cols).fill(null));
 
-  const roll = "@";
-  const threshold = 4;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       rollcount[y][x] =
@@ -34,17 +31,27 @@ const part1 = (rawInput: string) => {
           : getNeighbors(input, y, x).filter((n) => n === roll).length;
     }
   }
+  return rollcount.map((l) => l.map((c) => c < threshold));
+};
 
-  const accessible = rollcount
-    .flatMap((l) => l.map((c) => c < threshold))
-    .filter(Boolean);
-  return accessible.length;
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const accessible = mapAccessible(input);
+  return accessible.flat().filter(Boolean).length;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+  let plan = parseInput(rawInput);
+  let accessible = mapAccessible(plan);
+  let total = 0,
+    newlyAccessible = accessible.flat().filter(Boolean).length;
+  while (newlyAccessible > 0) {
+    total += newlyAccessible;
+    plan = plan.map((r, y) => r.map((c, x) => (accessible[y][x] ? "." : c)));
+    accessible = mapAccessible(plan);
+    newlyAccessible = accessible.flat().filter(Boolean).length;
+  }
+  return total;
 };
 
 const input = `
@@ -66,7 +73,7 @@ run({
     solution: part1,
   },
   part2: {
-    tests: [{ input, expected: "" }],
+    tests: [{ input, expected: 43 }],
     solution: part2,
   },
   trimTestInputs: true,
